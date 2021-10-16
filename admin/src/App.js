5a6df0acc8ad6648167e12113799afbbaef837de
Chu Nam Thắng <namthangmu123@gamil.com>
2021-10-17 02:41:53 +0700

    02:41 17/10/2021

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello ReactJS. This is my first project!
-        </p>
+        <p>Hello ReactJS. This is my first project!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
