0b222e7594dd2318c2b60585467d3689720c107d
Chu Nam Thắng <namthangmu123@gamil.com>
2021-10-21 12:12:08 +0700

    12:12 21/10/2021

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,20 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const clickMe = () => toast("Hello ReactJS!");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello ReactJS. This is my first project!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={clickMe}>Click Me!</button>
+        <ToastContainer />
       </header>
     </div>
   );
