c189d0d0360990662541b825ac8e4b67f32c9f97
Chu Nam Thắng <namthangmu123@gamil.com>
2021-10-25 01:12:33 +0700

    01:12 25/10/2021

--- admin/src/page/home/home.js
+++ admin/src/page/home/home.js
@@ -6,12 +6,33 @@ import logo from "../../assets/logo/logo322_116.png";
 import avatar from "../../assets/images/avatar.png"
 
 class Home extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    // openMenu = (event) => {
+    //     console.log(event.target.className);
+    // }
+
+
     render() {
+        // window.addEventListener("click", (event) => {
+        //     const className1 = "dropdown-menu-expand";
+
+        //     var toggle = event.target.dataset.toggle;
+        //     var content = document.getElementById(toggle);
+
+        //     if(content.classList.contains(className1)){
+        //         content.classList.remove(className1);
+        //     } else {
+        //         content.classList.add(className1);
+        //     }
+        // });
+
         return (
             <div className="home overlay-scrollbar light">
                 {/* Nav bar */}
                 <div className="navbar">
-
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <a href="# " className="nav-link">
@@ -32,11 +53,11 @@ class Home extends React.Component {
                     <ul className="navbar-nav nav-right">
                         <li className="nav-item dropdown">
                             <a href="# " className="nav-link">
-                                <i className="fas fa-bell"></i>
-                                <span className="navbar-badge">1</span>
+                                <i className="fas fa-bell dropdown-toggle" data-toggle="notification-menu" onClick={this.openMenu}></i>
+                                <span className="navbar-badge">7</span>
                             </a>
 
-                            <ul className="dropdown-menu-notification">
+                            <ul id="notification-menu" className="dropdown-menu-notification">
                                 <div className="dropdown-menu-header">
                                     <span>
                                         Notifications
@@ -198,21 +219,21 @@ class Home extends React.Component {
 
                         <li className="nav-item">
                             <div className="avatar dropdown">
-                                <img src={ avatar }  alt="Avatar" />
-                                <ul className="dropdown-menu-avatar">
-                                    <li  class="dropdown-menu-item">
-							            <a class="dropdown-menu-link">
+                                <img src={ avatar }  alt="Avatar" className="dropdown-toggle" data-toggle="user-menu"/>
+                                <ul id="user-menu" className="dropdown-menu-avatar">
+                                    <li className="dropdown-menu-item">
+							            <a href="# " className="dropdown-menu-link">
                                             <div>
-                                                <i class="fas fa-user-tie"></i>
+                                                <i className="fas fa-user-tie"></i>
                                             </div>
 								            <span>Profile</span>
 							            </a>
 						            </li>
 
-                                    <li  class="dropdown-menu-item">
-							            <a class="dropdown-menu-link">
+                                    <li className="dropdown-menu-item">
+							            <a href="# " className="dropdown-menu-link">
                                             <div>
-                                                <i class="fas fa-sign-out-alt"></i>
+                                                <i className="fas fa-sign-out-alt"></i>
                                             </div>
 								            <span>Logout</span>
 							            </a>
@@ -228,6 +249,86 @@ class Home extends React.Component {
 
                 {/* Slide bar */}
                 <div className="slidebar">
+                    <ul className="slidebar-nav">
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link active">
+                                <div><i className="fas fa-home"></i></div>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <h2>Account</h2>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i className="fas fa-user-circle"></i></div>
+                                <span>Admin Account</span>
+                            </a>
+                        </li>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i className="fas fa-user"></i></div>
+                                <span>Customer Account</span>
+                            </a>
+                        </li>
+
+                        <h2>Customer</h2>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i className="fas fa-user"></i></div>
+                                <span>Customer Information</span>
+                            </a>
+                        </li>
+
+                        <h2>Product</h2>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i class="fab fa-product-hunt"></i></div>
+                                <span>Product</span>
+                            </a>
+                        </li>
+
+                        <h2>Order</h2>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i className="fas fa-cart-plus"></i></div>
+                                <span>New Order</span>
+                            </a>
+                        </li>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i class="fas fa-clipboard-check"></i></div>
+                                <span>Order Confirmed</span>
+                            </a>
+                        </li>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i class="fas fa-archive"></i></div>
+                                <span>Package</span>
+                            </a>
+                        </li>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i class="fas fa-truck"></i></div>
+                                <span>Delivery</span>
+                            </a>
+                        </li>
+
+                        <li className="slidebar-nav-item">
+                            <a href="# " className="slidebar-nav-link">
+                                <div><i class="fas fa-check-circle"></i></div>
+                                <span>Order Success</span>
+                            </a>
+                        </li>
+                        
+                    </ul>
 
                 </div>
                 {/* End Slide bar */}
